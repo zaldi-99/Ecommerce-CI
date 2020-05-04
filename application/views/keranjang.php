7c5763574fba99ccacfe5ce400b9b2f692f13318
Rizky Zaldi <rizkyzaldi@gmail.com>
2020-05-05 05:44:57 +0700

    fitur checkout

--- application/views/keranjang.php
+++ application/views/keranjang.php
@@ -24,9 +24,14 @@
             <?php endforeach; ?>
                   <tr>  
                         <td colspan="4" align="center">Total</td>
-                        <td><?php echo number_format($this->cart->total(),0,',','.')  ?> </td>
+                        <td>Rp. <?php echo number_format($this->cart->total(),0,',','.')  ?> </td>
                   </tr>
-      
       </table>
+
+      <div align="right">
+            <a href="<?php echo base_url('Dashboard/hapus_keranjang') ?>"><div class="btn btn-sm btn-danger">Hapus Keranjang</div></a>
+            <a href="<?php echo base_url('Dashboard/index') ?>"><div class="btn btn-sm btn-primary">Lanjutkan Belanja</div></a>
+            <a href="<?php echo base_url('Dashboard/pembayaran') ?>"><div class="btn btn-sm btn-success">Bayar</div></a>
+      </div>
 
 </div>
---
+++ application/views/pembayaran.php
@@ -0,0 +1,69 @@
+<div class="container-fluid">
+      <div class="row">
+            <div class="col-md-2"></div>
+            <div class="col-md-8">
+                  <div class="btn btn-sm btn-danger">
+                        <?php $grand_total = 0;
+                        if ($keranjang = $this->cart->contents()) 
+                        {
+                              foreach ($keranjang as $item)
+                              {
+                                    $grand_total = $grand_total + $item['subtotal'];
+                              }
+                        echo "<h5>Total Belanja Anda : Rp. ". number_format($grand_total,0,',','.');
+                        
+                         ?>
+                  </div>
+
+                  <h3 class="mt-3">Input Alamat pengiriman dan Pembayaran</h3>
+                  <form methhod="post" action="<?php echo base_url('Dashboard/proses_pesanan') ?>">
+
+                        <div class="form-group">
+                              <label for="">Nama Lengkap</label>
+                              <input type="text" name="nama" placeholder="Nama Lengkap Anda" class="form-control">
+                        </div>
+
+                        <div class="form-group">
+                              <label for="">Alamat Lengkap</label>
+                              <input type="text" name="alamat" placeholder="Alamat Lengkap Anda" class="form-control">
+                        </div>
+
+                        <div class="form-group">
+                              <label for="">Nomor Telepon</label>
+                              <input type="text" name="no_tlp" placeholder="Nomor Telepon Anda" class="form-control">
+                        </div>
+
+                        <div class="form-group">
+                              <label for="">Kurir</label>
+                              <select class="form-control">
+                                    <option value="">JNE</option>
+                                    <option value="">Tiki</option>
+                                    <option value="">SiCepat</option>
+                                    <option value="">NinjaXpress</option>
+                              </select>
+                        </div>
+
+                        <div class="form-group">
+                              <label for="">Bank Transfer</label>
+                              <select class="form-control">
+                                    <option value="">BCA - XXXXX</option>
+                                    <option value="">BNI - XXXXX</option>
+                                    <option value="">Mandiri - XXXXX</option>
+                              </select>
+                        </div>
+
+                        <button type="submit" class="btn btn-sm btn-primary mb-3">Pesan</button>
+                        
+                  </form>
+                  <?php 
+                  }else {
+                        echo "Keranjang masih kosong";
+                  }
+                  ?>
+                  </div>
+            </div>
+
+            <div class="col-md-2"></div>
+      </div>
+
+</div>
